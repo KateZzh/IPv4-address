7d9bb90d1f28c4a8052f39576e86f932ad5c542f
KateZzh <ekaterin.pro2@gmail.com>
2023-08-17 18:55:37 +0300

    add axios

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [ip, setIp] = useState("");
+
+  async function requestServer() {
+    const response = await axios.get("https://api.ipify.org/?format=json");
+    setIp(response.data.ip);
+  }
+
+  useEffect(() => {
+    requestServer();
+  });
+
+  return (
+    <div className="App">
+      <h1>{ip}</h1>
+      <p>{ip} ( This is your IP address...probably :P )</p>
+    </div>
+  );
+}
+
+export default App;
